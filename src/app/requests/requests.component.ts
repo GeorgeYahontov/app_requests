36d35dfaade74407ceb6d23e829ab8c435031c2a
George <yaxontov1995@gmail.com>
2022-03-01 15:10:27 +0200

    Парамтры запросов

--- src/app/requests/requests.component.ts
+++ src/app/requests/requests.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Item} from "../item";
 
 @Component({
@@ -17,13 +17,34 @@ export class RequestsComponent implements OnInit {
   ngOnInit(): void {
   }
   getData():void{
-    this.http.get<Item[]>(this.itemsUrl).subscribe(
+    const url = this.itemsUrl
+    const headers = new HttpHeaders().set('Content-Type','aplication/json')
+
+    let params = new HttpParams()
+
+    if(this.item.id){
+      params = params.set('id',`${this.item.id}`)
+    }
+    if(this.item.name){
+      params = params.set('name',`${this.item.name}`)
+    }
+
+    const options = {headers, params}
+
+    // site/items => все записи
+    // site/items/id=2&name=ima => записи по запросу
+
+    this.http.get<Item[]>(url,options).subscribe(
       res => this.items = res,
       err => console.log('error',err)
     )
   }
   postData():void{
-    this.http.post<Item>(this.itemsUrl, this.item).subscribe(
+    const url = this.itemsUrl
+    const data = this.item
+    const headers = new HttpHeaders({'Content-Type':'aplication/json'})
+    const options = {headers}
+    this.http.post<Item>(url, data, options).subscribe(
       res => this.items.push(res),
       err => console.log(err)
     )
